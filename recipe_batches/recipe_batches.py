ba7c9d5dce303450e2d074d2fc1a3fb518ee0681
mark-marshall <mark.marshallgp@gmail.com>
2019-06-05 13:01:03 +0100

    Completed recipe_batches

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
 
-import math
+# Conditions
+# The keys will be the ingredient names, with their associated values being the amounts
+# Your function should output the maximum number of whole batches that can be made
 
-def recipe_batches(recipe, ingredients):
-  pass 
-
+def recipe_batches(recipe, inventory):
+  leftovers = []
+  # loop over the key/value pairs of the recipe dictionary
+  for recipe_key, recipe_value in recipe.items():
+    # divide each value by values in inventory
+    try:
+      leftovers.append(inventory[recipe_key] // recipe_value)
+    # if this item isn't in the inventory at all, return 0
+    except:
+      return 0
+  # if all ingredients exist in inventory, find the limiting ingredient
+  return min(n for n in leftovers)
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
