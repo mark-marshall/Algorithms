f7f9547becdc16ea18796617abf44ff78a1be3d8
mark-marshall <mark.marshallgp@gmail.com>
2019-06-05 13:04:06 +0100

    Completed eating_cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python
 
-import sys
+# Conditions
+# Cookie Monster can eat either 0, 1, 2, or 3 cookies at a time
+# If he were given n cookies, how many ways could he eat all cookies in the jar?
 
-# The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
-# recursive solution
-def eating_cookies(n, cache=None):
-  pass
+def eating_cookies(n):
+  # base cases according to rules
+  if n < 0:
+    return 0
+  elif n == 0 or n == 1:
+    return 1
+  elif n == 2:
+    return 2
+  elif n == 3:
+    return 4
+  # move towards base cases via 3 possible cookie denominations
+  return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
